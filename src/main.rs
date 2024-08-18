49da307dacfa7230bb82a297aa28bf183218ae14
maciejlewandowskii <maciej4612@gmail.com>
2024-08-18 18:42:52 +0200

    first working version of this program

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use dialoguer::{theme::ColorfulTheme, Select};
 use cargo_metadata::{ CargoOpt, MetadataCommand };
+use std::env;
+use std::process::Command;
+use std::os::unix::process::CommandExt;
 
 fn main() {
     let mut cmd = MetadataCommand::new();
@@ -8,19 +11,36 @@ fn main() {
 
     let metadata = cmd.exec().unwrap(); // if this fails, there is no reason to continue
 
+    // read aviable targets that user can choose
     let targets: Vec<&str> = metadata
         .packages
         .iter()
         .map(|t| t.name.as_str())
         .collect();
 
+    // show user selection prompt
     let selection = Select::with_theme(&ColorfulTheme::default())
         .with_prompt("Choose bin target to run: ")
         .default(0)
         .items(&targets[..])
         .interact()
         .unwrap();
+    let target = targets[selection];
 
-    println!("Running {} target via 'cargo run'!", targets[selection]);
- 
+    // run target via 'cargo run'
+    println!("Running {} target via 'cargo run'!", target);
+    Command::new("cargo")
+        .arg("run")
+        .arg("--bin") 
+        .arg(target) 
+        // change working directory to dir that program was called from
+        .current_dir(env::current_dir().unwrap())
+        /*
+            Becouse of this, this program will not work on windows.
+            why?: https://stackoverflow.com/a/53479765
+            
+            in summary: Execute 'cargo run' command and replace parent process
+            with process of this command.
+        */
+        .exec(); 
 }
